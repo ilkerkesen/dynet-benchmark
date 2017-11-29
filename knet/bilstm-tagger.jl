3879bde4d1fc44c5bfb031668cb8255b134b80e3
Ilker Kesen <ilkerksn@gmail.com>
2017-11-29 17:15:45 +0300

    port bilstm-tagger example to julia v0.6

--- knet/bilstm-tagger.jl
+++ knet/bilstm-tagger.jl
@@ -85,7 +85,7 @@ function main(args)
                     seq = make_input(sent, w2i)
                     nwords = length(sent)
                     ypred = predict(w, copy(s), seq)
-                    ypred = map(x->i2t[x], predict(w,copy(s),seq))
+                    ypred = map(x->i2t[x], ypred)
                     ygold = map(x -> x[2], sent)
                     same = true
                     for (y1,y2) in zip(ypred, ygold)
@@ -171,9 +171,9 @@ end
 # initialize hidden and cell arrays
 function initstate(atype, hidden, batchsize)
     state = Array(Any, 2)
-    state[1] = zeros(hidden, batchsize)
-    state[2] = zeros(hidden, batchsize)
-    return map(s->convert(atype,s), state)
+    state[1] = convert(atype, zeros(hidden, batchsize))
+    state[2] = convert(atype, zeros(hidden, batchsize))
+    return state
 end
 
 # initialize all weights of the language model
@@ -202,12 +202,12 @@ end
 function lstm(weight, bias, hidden, cell, input)
     gates   = weight * vcat(hidden,input) .+ bias
     hsize   = size(hidden,1)
-    forget  = sigm(gates[1:hsize,:])
-    ingate  = sigm(gates[1+hsize:2hsize,:])
-    outgate = sigm(gates[1+2hsize:3hsize,:])
-    change  = tanh(gates[1+3hsize:end,:])
+    forget  = sigm.(gates[1:hsize,:])
+    ingate  = sigm.(gates[1+hsize:2hsize,:])
+    outgate = sigm.(gates[1+2hsize:3hsize,:])
+    change  = tanh.(gates[1+3hsize:end,:])
     cell    = cell .* forget + ingate .* change
-    hidden  = outgate .* tanh(cell)
+    hidden  = outgate .* tanh.(cell)
     return (hidden,cell)
 end
 
